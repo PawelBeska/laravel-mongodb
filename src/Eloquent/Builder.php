35ffc600c8830a3962c87f02c67003118c9d5e1e
Arek <arkadiusz.wloka.dev@gmail.com>
2023-01-08 14:41:39 +0100

    Fix

--- src/Eloquent/Builder.php
+++ src/Eloquent/Builder.php
@@ -3,6 +3,7 @@
 namespace Jenssegers\Mongodb\Eloquent;
 
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Jenssegers\Mongodb\Collection;
 use Jenssegers\Mongodb\Helpers\QueriesRelationships;
 use MongoDB\Driver\Cursor;
 use MongoDB\Model\BSONDocument;
